0507eb7f68bb736faf48b6c8ae6ba17a4987a072
ley0s <lucas.gorgues31270@gmail.com>
2025-11-05 14:42:06 +0100

    TP3, Partie 3, ex5 pas commence

--- src/jeu/ZoneDeJeu.java
+++ src/jeu/ZoneDeJeu.java
@@ -4,19 +4,23 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import cartes.Attaque;
 import cartes.Bataille;
 import cartes.Borne;
 import cartes.Carte;
+import cartes.DebutLimite;
 import cartes.FinLimite;
 import cartes.Limite;
+import cartes.Parade;
+import cartes.Type;
 
 public class ZoneDeJeu {
 	private List<Limite> limites = new ArrayList<Limite>();
-	private List<Bataille> bataille = new ArrayList<Bataille>();
+	private List<Bataille> batailles = new ArrayList<Bataille>();
 	private List<Borne> bornes = new ArrayList<Borne>();
 	
 	public int donnerLimitationVitesse() {
-		if(limites.isEmpty() || limites. instanceof FinLimite)
+		if(limites.isEmpty() || limites.get(0) instanceof FinLimite)
 			return 200;
 		return 50;
 	}
@@ -34,8 +38,43 @@ public class ZoneDeJeu {
 		if(carte instanceof Borne borne)
 			bornes.add(borne);
 		if(carte instanceof Limite limite)
-			limites.add(limite);
-		if(carte instanceof Bataille cbataille)
-			bataille.add(cbataille);
+			limites.add(0,limite);
+		if(carte instanceof Bataille bataille)
+			batailles.add(0,bataille);
+	}
+	
+	public boolean peutAvancer() {
+		if(batailles.isEmpty())
+			return false;
+		if(batailles.get(0) instanceof Parade parade)
+			return parade.getType().equals(Type.FEU);
+		return false;
+	}
+	
+	public boolean estDepotFeuVertAutorise() {
+		if(batailles.isEmpty())
+			return true;
+		if(batailles.get(0) instanceof Attaque attaque)
+			return attaque.getType().equals(Type.FEU);
+		if(batailles.get(0) instanceof Parade parade)
+			return !parade.getType().equals(Type.FEU);
+		return false;
+	}
+	
+	public boolean estDepotBorneAutorise(Borne borne) {
+		return peutAvancer()
+			&& borne.getKm() > donnerLimitationVitesse()
+			&& borne.getKm()+donnerKmParcourus()<=1000;
+	}
+	
+	public boolean estDepotLimiteAutorise(Limite limite) {
+		if(limite instanceof DebutLimite)
+			return (limites.isEmpty() || (limites.get(0) instanceof FinLimite));
+		else
+			return (limites.get(0) instanceof DebutLimite);
+	}
+	
+	public boolean estDepotBatailleAutorise(Bataille bataille) {
+		
 	}
 }
